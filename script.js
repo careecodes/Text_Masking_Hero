db88a52e07985b7f2acad9e0cc6d7f1fe6800469
Caree Claunch <caree@caree.codes>
2018-12-22 09:57:09 -0600

    mouseleave text event

--- script.js
+++ script.js
@@ -11,13 +11,18 @@ $(document).ready(function(){
     $(document).mousemove(function(e){
         mouseX = e.pageX;
         mouseY = e.pageY;
-        console.log(mouseX + ',' + mouseY);
         traX = ((150 * mouseX) / 570) + 40;
         traY = ((150 * mouseY) / 570) + 50;
-        console.log(traX + ',' + traY);
         $(".hero-text").css({
             "background-position": traX + "%" + traY + "%",
             "background-size": "105% 105%"
+        });
+    });
+
+    $(document).mouseout(function(){
+        $(".hero-text").css({
+            "background-position": "center center",
+            "background-size": "cover"
         });
     });
   });
