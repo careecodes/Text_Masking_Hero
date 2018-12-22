eb6b39a4091cc373833a57cc2c9b69bf989f34ab
Caree Claunch <caree@caree.codes>
2018-12-22 09:37:50 -0600

    Changed speed of mouseover effect

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
         mouseX = e.pageX;
         mouseY = e.pageY;
         console.log(mouseX + ',' + mouseY);
-        traX = ((25 * mouseX) / 570) + 40;
-        traY = ((20 * mouseY) / 570) + 50;
+        traX = ((150 * mouseX) / 570) + 40;
+        traY = ((150 * mouseY) / 570) + 50;
         console.log(traX + ',' + traY);
         $(".hero-text").css({
             "background-position": traX + "%" + traY + "%",
-            "background-size": "110% 110%"
+            "background-size": "105% 105%"
         });
     });
   });
